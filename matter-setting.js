f0cf38f7d3f7642ccdbaee7e0ceae588e6949269
theo <theosylvoz@gmail.com>
2024-07-10 12:55:37 +0200

    fin des vacances

--- matter-setting.js
+++ matter-setting.js
@@ -1,96 +1,113 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.querySelector(".page");
+    const menuNav = document.querySelector("#menu-btn");
+    const menuMatter = document.querySelector("#matter-container");
+    const panelMenu = document.querySelector(".menu-panel-container");
 
     function toggleMenu() {
-        menuButton.classList.toggle("js-active");
-
-        if (menuButton.classList.contains("js-active")) {
-            // Open menu: initialize Matter.js
+        menuNav.classList.toggle("js-active");
+        if (menuNav.classList.contains("js-active")) {
+            menuMatter.classList.add("js-active");
+            panelMenu.classList.add("js-active");
+            removeOldCanvas(); // Supprime l'ancienne balise <canvas> avant de créer une nouvelle instance
             initializeMatter();
         } else {
-            // Close menu: destroy Matter.js
+            menuMatter.classList.remove("js-active");
+            panelMenu.classList.remove("js-active");
             destroyMatter();
         }
     }
 
-    menuButton.addEventListener("click", toggleMenu);
+    menuNav.addEventListener("click", toggleMenu);
 });
 
 let engine, render, runner, mouseConstraint;
+
+function removeOldCanvas() {
+    const oldCanvas = document.querySelector('#matter-container canvas');
+    if (oldCanvas) {
+        oldCanvas.remove();
+    }
+}
 
 function initializeMatter() {
     const THICCNESS = 120;
     const SVG_PATH_SELECTORS = ["#matter-path", "#matter-path2", "#matter-path3", "#matter-path4", "#matter-path5"];
     const matterContainer = document.querySelector("#matter-container");
+    const menuButton = document.querySelector(".menu-panel-container.js-active");
 
-    // module aliases
+    const desiredWidth = 350; // en pixels
+    const desiredHeight = window.innerHeight; // 100vh
+
+    // Créer un objet fictif pour simuler les dimensions souhaitées
+    const buttonRect = {
+        width: desiredWidth,
+        height: desiredHeight,
+        top: 0,
+        right: desiredWidth,
+        bottom: desiredHeight,
+        left: 0
+    };
+
+    const buttonWidth = buttonRect.width; // Utilisation de la largeur définie
+    const buttonHeight = buttonRect.height; // Utilisation de la hauteur définie
+
     var Engine = Matter.Engine,
         Render = Matter.Render,
         Runner = Matter.Runner,
         Bodies = Matter.Bodies,
         Composite = Matter.Composite,
-        Body = Matter.Body,
         Svg = Matter.Svg,
-        Vector = Matter.Vector,
         Vertices = Matter.Vertices,
-        Events = Matter.Events;
+        Events = Matter.Events,
+        Mouse = Matter.Mouse,
+        MouseConstraint = Matter.MouseConstraint;
 
-    // create an engine
     engine = Engine.create();
-
-    // create a renderer
     render = Render.create({
         element: matterContainer,
         engine: engine,
         options: {
-            width: 900,
-            height: 947,
+            width: buttonWidth,
+            height: buttonHeight,
             background: "transparent",
             wireframes: false,
             showAngleIndicator: false
         }
     });
 
-    createSvgBodies();
+    const panelMenu = document.querySelector(".menu-panel-container");
+    if (panelMenu.classList.contains("js-active")) {
+        createSvgBodies();
+    }
 
     let ground = Bodies.rectangle(
-        matterContainer.clientWidth / 2,
-        matterContainer.clientHeight + THICCNESS / 2,
-        1500,
+        buttonWidth / 2,
+        buttonHeight + THICCNESS / 2,
+        buttonWidth,
         THICCNESS,
-        {
-            isStatic: true,
-            render: { visible: false }
-        }
+        { isStatic: true, render: { visible: false } }
     );
 
     let leftWall = Bodies.rectangle(
         0 - THICCNESS / 2,
-        matterContainer.clientHeight / 2,
+        buttonHeight / 2,
         THICCNESS,
-        matterContainer.clientHeight * 5,
-        {
-            isStatic: true,
-            render: { visible: false }
-        }
+        24500, // Hauteur ajustée à 3000px
+        { isStatic: true, render: { visible: false } }
     );
 
     let rightWall = Bodies.rectangle(
-        349 + THICCNESS / 2,
-        matterContainer.clientHeight / 2,
+        buttonWidth + THICCNESS / 2,
+        buttonHeight / 2,
         THICCNESS,
-        matterContainer.clientHeight * 5,
-        {
-            isStatic: true,
-            render: { visible: false }
-        }
+        24500, // Hauteur ajustée à 3000px
+        { isStatic: true, render: { visible: false } }
     );
 
-    // add all of the bodies to the world
     Composite.add(engine.world, [ground, leftWall, rightWall]);
 
-    let mouse = Matter.Mouse.create(render.canvas);
-    mouseConstraint = Matter.MouseConstraint.create(engine, {
+    let mouse = Mouse.create(render.canvas);
+    mouseConstraint = MouseConstraint.create(engine, {
         mouse: mouse,
         constraint: {
             stiffness: 0.2,
@@ -102,7 +119,6 @@ function initializeMatter() {
 
     Composite.add(engine.world, mouseConstraint);
 
-    // allow scroll through the canvas
     mouseConstraint.mouse.element.removeEventListener(
         "mousewheel",
         mouseConstraint.mouse.mousewheel
@@ -112,40 +128,31 @@ function initializeMatter() {
         mouseConstraint.mouse.mousewheel
     );
 
-    // run the renderer
     Render.run(render);
-
-    // create runner
     runner = Runner.create();
-
-    // run the engine
     Runner.run(runner, engine);
-    console.log(Composite.allBodies(engine.world));
 
     function createSvgBodies() {
         const colors = ["red", "blue", "green", "purple", "orange"];
         const positions = [
-            { x: 200, y: -900 },
-            { x: 200, y: -400 },
-            { x: 300, y: -800 },
-            { x: 200, y: -600 },
-            { x: 300, y: -400 }
+            { x: buttonWidth / 2, y: -900 },
+            { x: buttonWidth / 2, y: -400 },
+            { x: buttonWidth / 2 + 100, y: -800 },
+            { x: buttonWidth / 2, y: -600 },
+            { x: buttonWidth / 2 + 100, y: -400 }
         ];
 
         SVG_PATH_SELECTORS.forEach((selector, index) => {
             const path = document.querySelector(selector);
 
             if (path) {
-                // Colorer l'élément SVG
                 path.setAttribute('fill', colors[index]);
-
                 let vertices = Svg.pathToVertices(path);
                 vertices = Vertices.scale(vertices, 1, 1);
                 let chamferedVertices = Vertices.chamfer(vertices, 1, 1, 1, 1);
-
                 let svgBody = Bodies.fromVertices(
-                    positions[index].x,  // Initial X position
-                    positions[index].y,  // Initial Y position
+                    positions[index].x,
+                    positions[index].y,
                     [chamferedVertices],
                     {
                         friction: 0.1,
@@ -174,20 +181,13 @@ function initializeMatter() {
             if (body) {
                 const { position, angle, bounds } = body;
 
-                // Calculer le centre du corps physique
-                const bodyCenterX = (bounds.min.x + bounds.max.x) / 2;
-                const bodyCenterY = (bounds.min.y + bounds.max.y) / 2;
-
-                // Obtenir les dimensions du SVG
                 const svgBBox = body.svgElement.getBBox();
                 const svgWidth = svgBBox.width;
                 const svgHeight = svgBBox.height;
 
-                // Calculer les offsets pour centrer le SVG
                 const offsetX = position.x - (svgWidth / 2);
                 const offsetY = position.y - (svgHeight / 2) + 100;
 
-                // Appliquer les transformations SVG pour aligner avec le centre du corps physique
                 body.svgElement.setAttribute(
                     'transform',
                     `translate(${offsetX}, ${offsetY}) rotate(${angle * (180 / Math.PI)}, ${svgWidth / 2}, ${svgHeight / 2})`
@@ -200,18 +200,28 @@ function initializeMatter() {
 }
 
 function destroyMatter() {
-    Render.stop(render);
-    Runner.stop(runner);
-    Composite.clear(engine.world, false);
-    Engine.clear(engine);
-
-    if (render.canvas) {
-        render.canvas.remove();
+    if (Render) {
+        Render.stop(Render);
+        if (Render.canvas) {
+            Render.canvas.remove();
+        }
+        if (Render.context) {
+            Render.context = null;
+        }
+        Render.textures = {};
     }
 
-    if (render.context) {
-        render.context = null;
+    if (Runner) {
+        Runner.stop(runner);
     }
 
-    render.textures = {};
+    if (Engine) {
+        Composite.clear(engine.world, false);
+        Engine.clear(engine);
+    }
+
+    Engine = null;
+    Render = null;
+    Runner = null;
+    mouseConstraint = null;
 }
